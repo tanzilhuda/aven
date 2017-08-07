4c8a5092b5e7fb2c698e61b470bfba8d7b7520b1
Tanzil Huda <tanzil30@gmail.com>
2017-08-07 10:46:36 +0600

    aven
    Aven: psd to html

---
+++ js/plugins_scroll.js
@@ -0,0 +1,13 @@
+/*plugins scroll*/
+/* ==============Full Page scrolbar======================*/
+
+(function($){
+			$(window).load(function(){
+				
+				$("body").mCustomScrollbar({
+					theme:"minimal"
+				});
+				
+			});
+		})(jQuery);
+/* ==============Full Page scrolbar======================*/
